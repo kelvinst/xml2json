f9353825df0fb04e6c08224fdd3fd1149fb11d1d
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-10-08 18:17:40 -0300

    First class!

---
+++ src/main/java/org/xml2json/Xml2Json.java
@@ -0,0 +1,13 @@
+package org.xml2json;
+
+import org.w3c.dom.Element;
+
+import com.google.gson.JsonObject;
+
+public class Xml2Json {
+
+	public static JsonObject convert(Element element) {
+		return null;
+	};
+	
+}
