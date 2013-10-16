82fa24935c1616c42069e71fd00225d85013c87f
Kelvin Raffael Stinghen <kelvin.stinghen@gmail.com>
2013-10-16 09:29:44 -0300

    A little bit more.

--- src/main/java/org/xml2json/Xml2Json.java
+++ src/main/java/org/xml2json/Xml2Json.java
@@ -1,15 +1,24 @@
 package org.xml2json;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import com.google.gson.JsonObject;
 
 public class Xml2Json {
 
-	public static JsonObject convert(Element element) {
+	public static JsonObject convert(Node node) {
 		JsonObject result = new JsonObject();
+		
+		if (node instanceof Element) {
+			Element element = (Element) node; 
+			
+			JsonObject object = new JsonObject();
+			result.add(element.getNodeName(), object);
+		}
 		
 		return result;
 	};
 	
 }
+
